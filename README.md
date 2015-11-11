12925830a5d56e482d1a62e15fd21ed18f7d86ca
Flinn Gendall <email": null>
2015-11-11 16:25:28 +1300

    add a bunch of commands with their sphero integration ripped out; update readme

--- README.md
+++ README.md
@@ -6,3 +6,14 @@ Because Taylor Swift isn't in your slack teams (yet)
 3. Create a new bot for your slack team at https://flinderstest.slack.com/services/new/bot
 4. Create a .env file based on .env-example and paste in your slack bot API token
 5. Run index.js on a server, or use `npm run devstart` to run using nodemon for testing new changes
+##Adding commands
+It is pretty easy to add your own commands for your tayBot. Simply add a new conditional after index.js:21 using the following form:
+```JavaScript
+    if(message.text.includes("YOUR KEYWORDS")){
+      response = "DESIRED RESPONSE"
+    }
+```
+If you want to make some more complex responses, try logging the message object to see what you can access. You can also check out the examples at https://github.com/slackhq/node-slack-client which this bot uses, although it is currently not very well documented.
+##Notes
+Current version is actually taken from a slack bot which was made to control a shpero, so some of the commands might not seem very sensible. Then again, this kind of bot isn't supposed to be very sensible anyway.
+There plans to move all of the commands into a separate file to make the whole thing easier to read.
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ slackToken = process.env.SLACK_KEY
 autoRecconect = true
 autoMark = true
 
+
 tayBot = new Slack(slackToken, autoRecconect, autoMark)
 
 tayBot.on('open', function(){
@@ -15,26 +16,56 @@ tayBot.on('open', function(){
 tayBot.on('message', function(message){
   channel = tayBot.getChannelGroupOrDMByID(message.channel)
   user = tayBot.getUserByID(message.user)
-  if(message.text.indexOf("hate")>-1){
-    response = "Haters gonna hate, hate, hate, hate, hate."
+  if(message.text.includes('tay')){
+
+    //===============ADD COMMANDS BENEATH THIS============
+
+    if(message.text.includes("hate")){
+      response = "Haters gonna hate, hate, hate, hate, hate."
+    }
+
+    else if(message.text.includes("play")){
+      response = "Players gonna play, play, play, play play."
+    }
+
+    else if(message.text.includes("fake")){
+      response = "Fakers gonna fake, fake, fake, fake, fake."
+    }
+
+    else if(message.text.includes("heartbreak")){
+      response = "Heartbreakers gonna break, break, break, break, break."
+    }
+
+    else if(message.text.includes("paint")){
+      var colour = null
+      if(message.text.includes('red')) colour = 'red'
+      else if(message.text.includes('orange')) colour = 'orange'
+      else if(message.text.includes('yellow')) colour = 'yellow'
+      else if(message.text.includes('green')) colour = 'green'
+      else if(message.text.includes('blue')) colour = 'blue'
+      else if(message.text.includes('purple')) colour = 'purple'
+      else if(message.text.includes('pink')) colour = 'pink'
+      if(colour){
+        response = 'Painting it '+colour+' for you'
+      }
+      else response = 'What colour would you like? I can paint it red, orange, yellow, green, blue, purple or pink!'
+    }
+
+    else if(message.text.includes('dance')){
+      response = 'I make the moves up as I go!'
+    }
+
+    else if(message.text.includes('wander')){
+      response = "I'm off for a stroll then!"
+    }
+
+    else if(message.text.includes('stop')){
+      response = "Can't stop, wont stop moving..."
+    }
+
+    else{response = "All you had to do was say something I'd understand..."}
+    channel.send(response)
   }
-  else if(message.text.indexOf("play")>-1){
-    response = "Players gonna play, play, play, play play."
-  }
-  else if(message.text.indexOf("fake")>-1){
-    response = "Fakers gonna fake, fake, fake, fake, fake."
-  }
-  else if(message.text.indexOf("heartbreak")>-1){
-    response = "Heartbreakers gonna break, break, break, break, break."
-  }
-  else if(message.text.indexOf("paint")>-1){
-    colour = '#ff0000'
-    // console.log(io)
-    io.emit('paint', colour)
-    response = 'Painting that for you'
-  }
-  else{response = "Sorry, I don't know what you mean"}
-  channel.send(response)
 })
 
 tayBot.on('error', function(err){
@@ -42,4 +73,3 @@ tayBot.on('error', function(err){
 })
 
 tayBot.login()
-
